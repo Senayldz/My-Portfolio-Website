74b505fb11bbd273381b62c782cde4e881c77696
Senayldz <senasenayildiz@gmail.com>
2024-07-04 12:00:15 +0300

    Added Navbar

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+
+import styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className={styles.App}>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
