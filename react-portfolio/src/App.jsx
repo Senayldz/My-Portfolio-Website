c92ca706569f04e24cc113b3a3b0cf15f5d7caca
Senayldz <senasenayildiz@gmail.com>
2024-07-04 12:30:25 +0300

    Update section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,12 +1,13 @@
 
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
-
+import {Hero} from './components/Hero/Hero'
 function App() {
 
   return (
     <div className={styles.App}>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -7,12 +7,11 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Sena</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a student in Suleyman Demirel University. Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="mailto:senasenayildiz@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
