a1fa85155c6bbcb8641d933c07f2270f73b24a0a
Senayldz <senasenayildiz@gmail.com>
2024-07-06 11:50:20 +0300

    Update Contact.jsx

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -11,15 +11,15 @@ export const Contact = () => {
             <ul className={styles.links}>
                 <li className={styles.link}>
                     <img src={getImageUrl("contact/emailIcon.png")} alt='email icon' />
-                    <a href='mailto:myemail@emil.com'>myemail@emil.com</a>
+                    <a href='mailto:senasenayildiz@gmail.com'>senasenayildiz@gmail.com</a>
                 </li>
                 <li className={styles.link}>
                     <img src={getImageUrl("contact/linkedinIcon.png")} alt='linkedin icon' />
-                    <a href='https://www.linkedin.com/in/sena-y%C4%B1ld%C4%B1z-53a017209/'>linkedin.com/my name</a>
+                    <a href='https://www.linkedin.com/in/sena-y%C4%B1ld%C4%B1z-53a017209/'>linkedin.com/sena-yıldız</a>
                 </li>
                 <li className={styles.link}>
                     <img src={getImageUrl("contact/githubIcon.png")} alt='github icon' />
-                    <a href='https://github.com/Senayldz'>myemail@emil.com</a>
+                    <a href='https://github.com/Senayldz'>Senayldz</a>
                 </li>
             </ul>
         </footer>
